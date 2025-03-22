fb8b362997136fef27b81e1510c68de3527d34fc
Harsh-v3 <harshwebdev015@gmail.com>
2025-03-22 08:24:31 +0530

    links are added

--- script.js
+++ script.js
@@ -1,72 +1,3 @@
-// const linksContainer = document.querySelector(".popup__content");
-// const popupBtns = document.getElementsByClassName("access");
-// console.log(linksContainer, popupBtns);
-
-// const popupBody = function (i) {
-//   linksContainer.innerHTML = ``;
-
-//   let heading;
-//   if (i === 0) {
-//     heading = `BOOKS & NOTES`;
-//   } else if (i === 1) {
-//     heading = "QUESTION PAPERS";
-//   } else if (i == 2) {
-//     // heading = "ASSIGNMENT";
-//     return problemBody();
-//   } else {
-//     // heading = "RESULT";
-//     return problemBody();
-//   }
-//   html = `
-//     <div class="popup__right">
-//             <div class="popup__close">
-//               <a href="#features" class="popup__close--btn">&times;</a>
-//             </div>
-//             <h2 class="heading-secondary u-margin-bottom-small">${heading}</h2>
-//             <h3 class="heading-tertiary u-margin-bottom-small">
-//               BCA 6th Semester ${
-//                 i === 1 ? `, Last 5 Years Question Papers !` : ``
-//               }
-//             </h3>
-//             <div class="popup__text u-margin-bottom-medium">
-//               <div class="popup__links">
-//               <a href="#" class="btn btn--green">BCA - 361</a>
-//                 <a href="#" class="btn btn--green">BCA - 362</a>
-//                 <a href="#" class="btn btn--green">BCA - 363</a>
-//                 <a href="#" class="btn btn--green">BCA - 364</a>
-//                 <a href="#" class="btn btn--green">BCA - 365</a>
-//                 <a href="#" class="btn btn--green">BCA - 366</a></div>
-//             </div>
-//           </div>
-//     `;
-//   return html;
-// };
-
-// const problemBody = function () {
-//   linksContainer.innerHTML = ``;
-//   html = `<div class="popup__right">
-//             <div class="popup__close">
-//               <a href="#features" class="popup__close--btn">&times;</a>
-//             </div>
-//             <h2 class="heading-secondary u-margin-bottom-small">Login Required !</h2>
-//           </div>`;
-
-//   return html;
-// };
-
-// popupBtns[0].addEventListener("click", function () {
-//   linksContainer.insertAdjacentHTML("beforeend", popupBody(0));
-// });
-// popupBtns[1].addEventListener("click", function () {
-//   linksContainer.insertAdjacentHTML("beforeend", popupBody(1));
-// });
-// popupBtns[2].addEventListener("click", function () {
-//   linksContainer.insertAdjacentHTML("beforeend", popupBody(2));
-// });
-// popupBtns[3].addEventListener("click", function () {
-//   linksContainer.insertAdjacentHTML("beforeend", popupBody(3));
-// });
-
 ("use strict");
 
 const openButton = document.querySelectorAll(".open-modal");
@@ -74,12 +5,14 @@ const closeButton = document.querySelector(".close-modal");
 const modalWindow = document.querySelector(".modal-window");
 const overlay = document.querySelector(".overlay");
 const container = document.querySelector(".cont");
+
 let currPopup;
 
 console.log(openButton);
 console.log(closeButton);
 console.log(modalWindow);
 console.log(overlay);
+console.log(container);
 
 openButton[0].addEventListener("click", function (i) {
   currPopup = 0;
@@ -93,6 +26,101 @@ openButton[2].addEventListener("click", function (i) {
 openButton[3].addEventListener("click", function (i) {
   currPopup = 3;
 });
+
+const insertBody = function () {
+  container.innerHTML = ``;
+  const heading = ["Books & Notes", "Question Papers", "Assignments", "Result"];
+  const about = [
+    "Here is PDFs of all subjects expect OS-2 !",
+    "Here you can get last 5 years Question Papers !",
+    "For Last Two Assignments Contact Developers (+91 9518816505) !",
+    "Result of Various Semesters ... ",
+  ];
+  const links = [
+    [
+      "https://www.mediafire.com/file/qz8rykcqop6argm/Computer_Graphics__BCA__6th_Sem_Book.pdf/file",
+      "https://www.mediafire.com/file/1xnxacy572sp6a0/Internet_Technology_book.pdf/file",
+      "https://www.mediafire.com/file/kidg31k4ouvy9j6/WD_in_AT__BCA__book.pdf/file",
+      "https://www.mediafire.com/file/5hcsvb7n7j70tjb/Operating_Oystem_Sem6_notes.pdf/file",
+      "https://www.mediafire.com/file/6rue09a9rgukjxv/Advanced_Programming_in_VB_book.pdf/file",
+      "https://www.mediafire.com/file/ho7gk2sxeyvqo45/Java_Sem6_Book_.pdf/file",
+    ],
+    [
+      "https://www.mediafire.com/file/2phc4u0diqr19co/CGqpSem6.zip/file",
+      "https://www.mediafire.com/file/n8ab56wr4ea4e3s/ITqpSem6.zip/file",
+      "https://www.mediafire.com/file/y3gc7bjmdx6473n/WD-USING-AT-qp_Sem6.zip/file",
+      "https://www.mediafire.com/file/kzhqibi000okmtq/OS-II_qpSem6.zip/file",
+      "https://www.mediafire.com/file/nsqes9agc34wg48/AP-WITH-VB-qpSem6.zip/file",
+      "https://www.mediafire.com/file/7ankf94e8werliw/JAVA_qpSem6.zip/file",
+    ],
+    [
+      "",
+      "https://www.mediafire.com/file/xc2flirltpc9xjy/Computer_Graphics_Assignment_sem6.pdf/file",
+      "https://www.mediafire.com/file/xs8w69x59j7unan/VB_6.0.pdf/file",
+      "",
+      "",
+    ],
+    [],
+  ];
+  const body = [
+    `<div class="btns--container">
+            <a href="${links[2][0]}" class="btn-text btnLink btn--1">OS (HandWritten)!</a>
+            <a href="${links[2][1]}" class="btn-text btnLink btn--1">CG (HandWritten)!</a>
+            <a href="${links[2][2]}" class="btn-text btnLink btn--2">APVB (HandWritten)!</a>
+            <a href="#" class="btn-text btnLink btn--3">Web Dev (PDF/Printout)!</a>
+            <a href="#" class="btn-text btnLink btn--4">APVB (PDF/Printout)!</a>
+            
+        </div>`,
+    `<div class="btns--container">
+            <a href="${
+              currPopup == 0 ? links[0][0] : links[1][0]
+            }" target='_blank' class="btn-text btnLink btn--1">CG</a>
+            <a href="${
+              currPopup == 0 ? links[0][1] : links[1][1]
+            }" target='_blank' class="btn-text btnLink btn--2">IT</a>
+            <a href="${
+              currPopup == 0 ? links[0][2] : links[1][2]
+            }" target='_blank' class="btn-text btnLink btn--3">Web Dev</a>
+            <a href="${
+              currPopup == 0 ? links[0][3] : links[1][3]
+            }" target='_blank' class="btn-text btnLink btn--4">OS-II</a>
+            <a href="${
+              currPopup == 0 ? links[0][4] : links[1][4]
+            }" target='_blank' class="btn-text btnLink btn--5">APVB</a>
+            <a href="${
+              currPopup == 0 ? links[0][5] : links[1][5]
+            }" target='_blank' class="btn-text btnLink btn--6">JAVA</a>
+        </div>`,
+    `<div class="btns--container">
+            <a href="#" class="btn-text btnLink btn--1">Sem 1</a>
+            <a href="#" class="btn-text btnLink btn--2">Sem 2</a>
+            <a href="#" class="btn-text btnLink btn--3">Sem 3</a>
+            <a href="#" class="btn-text btnLink btn--4">Sem 4</a>
+            <a href="#" class="btn-text btnLink btn--5">Sem 5</a>
+            <a href="#" class="btn-text btnLink btn--6">Sem 6</a>
+            
+        </div>`,
+  ];
+  html = `
+          <h3 class="u-center-text u-margin-bottom-small heading-secondary">
+            ${heading[currPopup]}
+          </h3>
+          <h5 class="u-center-text heading-tertiary u-margin-bottom-medium">
+            ${about[currPopup]}
+          </h5>
+          ${
+            currPopup == 2
+              ? body[0]
+              : !currPopup
+              ? body[1]
+              : currPopup === 3
+              ? body[2]
+              : body[1]
+          }
+            
+  `;
+  container.insertAdjacentHTML("beforeend", html);
+};
 
 const openModal = function () {
   insertBody();
@@ -119,45 +147,3 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
-
-const insertBody = function () {
-  container.innerHTML = ``;
-  console.log(currPopup);
-  let heading = currPopup === 1 ? `Question Papers` : `Books & Notes`;
-
-  html = `
-      <div class="popup__right">
-              <div class="popup__close">
-                <a href="#features" class="popup__close--btn">&times;</a>
-              </div>
-              <h2 class="heading-secondary u-margin-bottom-small u-center-text">${heading}</h2>
-              ${
-                currPopup > 1
-                  ? `
-                  <h3 class="heading-tertiary u-margin-bottom-small u-center-text">  Sorry This Content is Not available for everyone for now !
-              </h3>
-              <div >
-                <div class="popup__links u-center-text">
-                Contact : 9518816505
-              </div>`
-                  : `<h3 class="heading-tertiary u-margin-bottom-small u-center-text">
-                BCA 6h Sem ${
-                  currPopup === 1 ? `, Lat 5 Year Question Papers !` : ` `
-                }
-                
-              </h3>
-              <div >
-                <div class="popup__links">
-                <a href="#" class="btn-text ">BCA - 361</a>
-                  <a href="#" class="btn-text ">BCA - 362</a>
-                  <a href="#" class="btn-text ">BCA - 363</a>
-                  <a href="#" class="btn-text ">BCA - 364</a>
-                  <a href="#" class="btn-text ">BCA - 365</a>
-                  <a href="#" class="btn-text ">BCA - 366</a></div>
-              </div>`
-              }
-            </div>
-      `;
-
-  container.insertAdjacentHTML("beforeend", html);
-};
