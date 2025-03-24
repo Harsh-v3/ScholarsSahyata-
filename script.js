f10071bc97981abf7df1e36ecf59feea6e1e3e8a
Harsh-v3 <harshwebdev015@gmail.com>
2025-03-24 20:30:12 +0530

    update

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ const insertBody = function () {
       "https://www.mediafire.com/file/sbrruaq0y26e3ub/Internet_Technology_sem6_Book_.pdf/file",
       "https://www.mediafire.com/file/kidg31k4ouvy9j6/WD_in_AT__BCA__book.pdf/file",
       "https://www.mediafire.com/file/5hcsvb7n7j70tjb/Operating_Oystem_Sem6_notes.pdf/file",
-      "https://www.mediafire.com/file/6rue09a9rgukjxv/Advanced_Programming_in_VB_book.pdf/file",
+      "https://www.mediafire.com/file/sonzp3rmxfy5fzy/advd_Prog_in_VB.pdf/file",
       "https://www.mediafire.com/file/ho7gk2sxeyvqo45/Java_Sem6_Book_.pdf/file",
     ],
     [
@@ -91,10 +91,9 @@ const insertBody = function () {
         </div>`,
     `<div class="btns--container">
             <a href="#" class="item" onclick="tt()" >Time Table</a>
-            <a href="#" class="item" onclick="downloadVbFp()" >VB Front Page</a>
-            <a href="#" class="item" onclick="downloadVbPdf()" >VB Practicle File</a>
-            <a href="#" class="item">Result</a>
-            <a href="#" class="item">Special Notes</a>
+            <a href="#" class="item" onclick="downloadFile('VBFrontPage')" >VB Front Page</a>
+            <a href="#" class="item" onclick="downloadFile('VBAssignment')" >VB Practicle File</a>
+            <a href="#" class="item" onclick="downloadFile('DateSheet')">Datesheet</a>
             <a href="#" class="item">Importent Questions</a>
             <a href="#" class="item">Sllyabus</a>
           </div>`,
@@ -147,34 +146,25 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
-const insertTt = function () {
+function tt() {
+  modalWindow.style.width = "auto";
   container.innerHTML = ``;
   let body = `<img src="Resourses/images/TimeTable.jpg" alt="Time Table image" />`;
   container.insertAdjacentHTML("beforeend", body);
-};
-
-function tt(e) {
-  modalWindow.style.width = "auto";
-  insertTt();
   modalWindow.classList.remove("hidden");
   overlay.classList.remove("hidden");
 }
 
-function downloadVbPdf() {
-  console.log("hi");
-  const link = document.createElement("a");
-  link.href = "Resourses/pdfs/vb.pdf"; // Replace with your PDF URL
-  link.download = "vb.pdf"; // Set the filename
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-}
-function downloadVbFp() {
-  console.log("hi");
-  const link = document.createElement("a");
-  link.href = "Resourses/pdfs/frontPage.pdf"; // Replace with your PDF URL
-  link.download = "frontPage.pdf"; // Set the filename
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
+function downloadFile(fileName) {
+  let userChoice = confirm(`Do you want to download ${fileName} file ?`);
+  if (userChoice) {
+    const link = document.createElement("a");
+    link.href = `Resourses/pdfs/${fileName}.pdf`; // Replace with your PDF URL
+    link.download = `${fileName}.pdf`; // Set the filename
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  } else {
+    return;
+  }
 }
